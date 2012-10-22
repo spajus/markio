714f37127db470cb0f6d84afaa3710bcc69726cb
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2012-10-22 08:14:12 +0300

    Parsing folders, some troubles with nesting though.

--- lib/markio/parser.rb
+++ lib/markio/parser.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'date'
 class Markio::Parser
 
   include Enumerable
@@ -10,13 +11,12 @@ class Markio::Parser
 
   def each
     node = @document.at_xpath('//html/body')
-    @folders = []
-    traverse(node) { |bookmark| yield bookmark }
+    traverse(node, []) { |bookmark| yield bookmark }
   end
 
   private
 
-  def traverse(node, &block)
+  def traverse(node, folders, &block)
     bookmarks = node.search './dt//a'
     folder_names = node.search './dt/h3'
     folder_items = node.search './dl'
@@ -25,17 +25,25 @@ class Markio::Parser
       bookmark = Markio::Bookmark.new
       bookmark.title = data.text
       bookmark.href = data['href']
-      bookmark.folders = Array.new(@folders)
+      bookmark.folders = Array.new(folders)
+      bookmark.add_date = parse_timestamp data['add_date']
+      bookmark.last_visit = parse_timestamp data['last_visit']
+      bookmark.last_modified = parse_timestamp data['last_modified']
       yield bookmark
     end
 
     folder_items.size.times do |i|
       folder_name = folder_names[i]
       folder_item = folder_items[i]
-      @folders << folder_name unless folder_name.nil?
-      traverse(folder_item, &block)
+      next_folders = Array.new(folders)
+      next_folders << folder_name.text unless folder_name.nil?
+      traverse(folder_item, next_folders, &block)
     end
 
+  end
+
+  def parse_timestamp(timestamp)
+    Time.at(timestamp.to_i).to_datetime if timestamp
   end
 
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Markio::Parser do
+
   it 'should parse single bookmark' do
     parser = Markio::Parser.new("spec/test_data/one_bookmark.html")
     bookmarks = []
@@ -13,5 +14,18 @@ describe Markio::Parser do
     bookmark.title.should eq 'Save It - Simple, secure bookmarks'
     bookmark.href.should eq 'https://saveit.in/'
     bookmark.folders.length.should eq 0
+    bookmark.add_date.should_not be_nil
+    bookmark.last_visit.should_not be_nil
+    bookmark.last_modified.should_not be_nil
+    bookmark.add_date.class.should be DateTime
+    bookmark.last_visit.class.should be DateTime
+    bookmark.last_modified.class.should be DateTime
   end
+
+  it 'should parse multiple bookmarks' do
+    parser = Markio::Parser.new("spec/test_data/many_bookmarks.html")
+    bookmarks = []
+    parser.each { |b| bookmarks << b }
+  end
+
 end
